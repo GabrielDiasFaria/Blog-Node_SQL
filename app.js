d2200f07268a1019618e4eeb0c2c810b563a05c1
GabrielDiasFaria <gabrieldiasfaria@gmail.com>
2018-05-29 07:38:16 -0300

    Alterando Git Ignore
    Testando GitIgnore

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var app = require('./config/server');
 
 // ---------------------- START SERVER -----------------------
 app.listen(3000, function(){
-    console.log("||=============================|| ");
-    console.log("||====> Servidor iniciado <====|| ");
-    console.log("||=============================|| ");
+    console.log("||=============================||");
+    console.log("||====> Servidor iniciado <====||");
+    console.log("||=============================||");
 });
